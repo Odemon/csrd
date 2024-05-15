a964b7d182c563738188668f134e8e696b4bf74a
Otso Leppälä <otso.leppala@gmail.com>
2024-05-15 11:19:58 +0300

    Fetch json data from s3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
+import { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    
+    this.state = {
+      csrd_sectors : []
+    };
+  }
+  componentDidMount() {
+    fetch('https://otso-csrd.s3.eu-west-1.amazonaws.com/json/esr_sectors.json')
+      .then((response) => response.json())
+      .then((sectors) =>
+        this.setState(
+          () => {
+            return {csrd_sectors : sectors};
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
+  }
+  render() {
+    return (
+      <div className="App">
+        {this.state.csrd_sectors.map((sector) => {
+          return <h1 key={sector.id}>{sector.sector}</h1>
+        })}
+      </div>
+    );    
+  }
 }
 
 export default App;
